bc5838c6e9f49e1542e107559d87724527f7f8ae
Aditi Pingoliya <pingolia2007@gmail.com>
2026-05-29 19:45:46 +0530

    Add files via upload

---
+++ sorted.cpp
@@ -0,0 +1,31 @@
+//Check if array is sorted or not
+#include <iostream>
+using namespace std;
+
+int sortedArray(int arr[], int n){
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int arr[] = {1,3,2,3,1,4};
+    int n = 6;
+
+    sortedArray(arr , n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    if(sortedArray(arr, n)){
+        cout<<"sorted"<<" ";
+    }
+    else {
+        cout<<"not sorted"<<" ";
+    }
+    return 0;
+}
+
