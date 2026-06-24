e86e5916f06a16f44b824d9ab85cd69a126ec6cf
Aditi Pingoliya <pingolia2007@gmail.com>
2026-06-24 18:14:33 +0530

    #724 find pivot index

--- Arrays.cpp
+++ Arrays.cpp
@@ -150,3 +150,20 @@ long long maxSubarraySum(int arr[], int n) {
         }
         return ans;
     }
+
+//724 Find Pivot index
+int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
+        }
+        int leftSum = 0;
+        for(int i=0;i<nums.size();i++){
+            int rightSum = totalSum - leftSum - nums[i];
+            if(leftSum==rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
