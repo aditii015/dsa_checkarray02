49bc95a517fca72b0701fbc00ddcce85d93a5583
Aditi Pingoliya <pingolia2007@gmail.com>
2026-06-26 16:42:52 +0530

    #31 Next Permutation

--- Arrays.cpp
+++ Arrays.cpp
@@ -183,3 +183,26 @@ int maxProduct(vector<int>& nums) {
         }
         return ans;
     }
+
+//31 Next Permutation
+void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for(int i=n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin() + index + 1, nums.end());      
+    }
