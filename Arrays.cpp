3ef3751043e1c06abfd4891ea8a97cd4a4e0b0db
Aditi Pingoliya <pingolia2007@gmail.com>
2026-07-05 16:21:59 +0530

    #199 Right Side View in B.T.

--- Arrays.cpp
+++ Arrays.cpp
@@ -327,3 +327,24 @@ vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         }
         return ans;
     }
+
+//199 Right Side View in B.T.
+vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();            
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(i == size-1){
+                    ans.push_back(node->val);
+                }
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        return ans;
+    }
