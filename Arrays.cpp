d6deaf22db54933f89784e2e88fc5d0364e6b644
Aditi Pingoliya <pingolia2007@gmail.com>
2026-07-07 16:39:27 +0530

    #110 Balanced BT

--- Arrays.cpp
+++ Arrays.cpp
@@ -379,3 +379,21 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
             return lowestCommonAncestor(root->right, p, q);
         return root;
     }
+
+//110 Balanced BT
+int height(TreeNode* root){
+        if(root==NULL) return 0;
+        
+        int left = height(root->left);
+        if(left == -1) return -1;
+
+        int right = height(root->right);
+        if(right == -1) return -1;
+
+        if(abs(left - right) > 1) return -1;
+
+        return 1 + max(left, right);
+    }
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
+    }
