ec5e436bc678e88d6d01ad9d382d60ca020ae7c8
Aditi Pingoliya <pingolia2007@gmail.com>
2026-07-17 16:37:02 +0530

    #2643 Find Row with Maximum 1's

--- Arrays.cpp
+++ Arrays.cpp
@@ -493,3 +493,21 @@ int findPeakElement(vector<int>& nums) {
         }
         return low;
     }
+
+//2643 Find Row with Maximum 1's
+vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+       int maxOnes = 0;
+        int rowIndex = 0;
+       
+        for(int i=0;i < mat.size();i++){
+            int count = 0;
+            for(int j=0;j<mat[i].size();j++){
+                if(mat[i][j] == 1) count ++;
+            }
+            if(count>  maxOnes){
+                maxOnes = count;
+                rowIndex = i;
+            }
+        }
+        return {rowIndex, maxOnes};
+    }
