6590af6e01fbccc7cc56a5225c33f5385f90b5c2
Aditi Pingoliya <pingolia2007@gmail.com>
2026-07-22 17:30:16 +0530

    #151 Reverse Words in a string

--- Arrays.cpp
+++ Arrays.cpp
@@ -529,3 +529,25 @@ string removeOuterParentheses(string s) {
         }
         return result;
     }
+
+//151 Reverse Words in a string
+string reverseWords(string s) {
+        string result = "";
+        int i = s.size()-1;
+        while(i >= 0){
+            while(i>=0 && s[i] == ' '){
+                i--;
+            }
+            if(i<0) break;
+            int end = i;
+            while(i >= 0 && s[i] != ' '){
+                i--;
+            }
+            string word = s.substr(i + 1, end - i);
+            if(!result.empty()){
+                result += " ";
+            }
+            result += word;
+        }
+        return result;
+    }
