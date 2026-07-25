074d41b3e26bfe84313822c6708b95ca6f4eb185
Aditi Pingoliya <pingolia2007@gmail.com>
2026-07-25 18:23:20 +0530

    #1781 Sum of Beauty of all substrings

--- Arrays.cpp
+++ Arrays.cpp
@@ -662,3 +662,23 @@ int romanToInt(string s) {
         } 
         return res + roman[s.back()];
     }
+
+//1781 Sum of Beauty of all substrings
+int beautySum(string s) {
+        int n = s.length();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            unordered_map<char, int> freq;
+            for(int j=i;j<n;j++){
+                freq[s[j]]++;          
+                int mini = INT_MAX;
+                int maxi = INT_MIN;
+                for(auto it : freq){
+                    mini = min(mini, it.second);
+                    maxi = max(maxi, it.second);
+                }
+                sum += (maxi - mini);
+            }
+        }
+        return sum;
+    }
