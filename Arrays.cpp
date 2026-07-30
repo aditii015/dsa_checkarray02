cc64394cea1b8bd4d730cf0f05516631971ab388
Aditi Pingoliya <pingolia2007@gmail.com>
2026-07-30 18:01:15 +0530

    #66 Plus One(array)

--- Arrays.cpp
+++ Arrays.cpp
@@ -712,3 +712,17 @@ int myAtoi(string s) {
         }
         return sign * num;
     }
+
+//66 Plus One(array)
+vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
