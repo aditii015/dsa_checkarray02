062ced6e7e7db297f010456bf42220a2d3775b99
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-01 17:18:48 +0530

    #20 Valid Paranthese(stack)

--- Arrays.cpp
+++ Arrays.cpp
@@ -754,3 +754,25 @@ int majorityElement(vector<int>& nums) {
         }
         return candidate;
     }
+
+//20 Valid Paranthese(stack)
+bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+
+                if(ch == ')' && st.top() != '(')
+                   return false;
+                if(ch == '}' && st.top() != '{')
+                   return false;
+                if(ch == ']' && st.top() != '[')
+                   return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
