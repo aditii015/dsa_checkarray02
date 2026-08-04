731c510fab56ce015e67632ffcf99f90e839659a
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-04 19:20:00 +0530

    #21 Merge Two Sorted Lists

--- Arrays.cpp
+++ Arrays.cpp
@@ -776,3 +776,26 @@ bool isValid(string s) {
         }
         return st.empty();
     }
+
+//21 Merge Two Sorted Lists
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode current(-1);
+        ListNode* tail = &current;
+
+        while(list1 != NULL && list2 != NULL) {
+            if(list1->val <= list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+        if(list1 != NULL)
+            tail->next = list1;
+        if(list2 != NULL)
+            tail->next = list2;
+        return current.next;
+    }
