2bdaaad535d9361563e96a21e57afe848139b40b
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-06 08:05:45 +0530

    #125 Valid Palindrome(two pointer)

--- Arrays.cpp
+++ Arrays.cpp
@@ -829,3 +829,21 @@ int strStr(string haystack, string needle) {
         }
         return -1;
     }
+
+//125 Valid Palindrome(two pointer)
+bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while(left < right){
+            while(left < right && !isalnum(s[left]))
+               left++;
+            while(left < right && !isalnum(s[right]))
+               right--;
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
