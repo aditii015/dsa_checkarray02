b35ba8d80fbec5d6c536dac66941fbf702a1ed32
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-09 07:02:20 +0530

    #142 Linked List cycle II

--- Arrays.cpp
+++ Arrays.cpp
@@ -876,3 +876,24 @@ bool hasCycle(ListNode *head) {
         }
         return false;
     }
+
+//142 Linked List cycle II
+ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast -> next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
