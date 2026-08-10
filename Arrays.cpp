400b45a7d3bcb896c189ea716e0a0e27cb27ecf7
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-10 06:44:46 +0530

    #290 Word Pattern

--- Arrays.cpp
+++ Arrays.cpp
@@ -897,3 +897,33 @@ ListNode *detectCycle(ListNode *head) {
         }
         return NULL;
     }
+
+//290 Word Pattern
+bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+
+        while(ss >> word){
+            words.push_back(word);
+        }
+        if(pattern.size() != words.size()){
+            return false;
+        }
+        unordered_map<char, string> mp1;
+        unordered_map<string, char> mp2;
+
+        for(int i=0;i<pattern.size();i++){
+            char ch = pattern[i];
+            string w = words[i];
+
+            if(mp1.count(ch) && mp1[ch] != w)
+                return false;
+            if(mp2.count(w) && mp2[w] != ch)
+                return false;
+
+            mp1[ch] = w;
+            mp2[w] = ch; 
+        }
+        return true;
+    }
