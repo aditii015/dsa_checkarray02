be6719a5c5965073b26e0d10df430575658719ef
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-12 06:58:04 +0530

    #19 Remove Nth node from end of LL

--- Arrays.cpp
+++ Arrays.cpp
@@ -964,3 +964,19 @@ ListNode* reverseList(ListNode* head){
         slow->next = reverseList(second);
         return true;
     }
+
+//19 Remove Nth node from end of LL(slow & fast approach)
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* slow  = dummy;
+        ListNode* fast = dummy;
+        for(int i=0;i<n;i++){
+            fast = fast->next;
+        }
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
