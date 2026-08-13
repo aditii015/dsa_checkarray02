1f5ab909ea9009bc67f8212e398eff2e990b1333
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-13 07:30:16 +0530

    #2095 Delete the Middle Node of LL

--- Arrays.cpp
+++ Arrays.cpp
@@ -991,3 +991,21 @@ ListNode* middleNode(ListNode* head) {
         }
         return slow;
     }
+
+//2095 Delete the Middle Node of LL
+ListNode* deleteMiddle(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next->next;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* middle = slow->next;
+        slow->next = slow->next->next;
+        delete middle;
+        return head;
+    }
