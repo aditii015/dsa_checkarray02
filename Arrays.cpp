291adf5a977eba3aab048c478dd6ad4208984ace
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-14 07:03:10 +0530

    #148 Sort List

--- Arrays.cpp
+++ Arrays.cpp
@@ -1009,3 +1009,55 @@ ListNode* deleteMiddle(ListNode* head) {
         delete middle;
         return head;
     }
+
+//148 Sort List(merge sort)
+ListNode* mergeTwoSortedLL(ListNode* list1, ListNode* list2){
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val <= list2->val){
+                temp ->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp -> next;
+        }
+        if(list1 != NULL){
+            temp->next = list1;
+        }
+        else{
+            temp->next = list2;
+        }
+        return dummyNode->next;
+    }
+
+    ListNode* findMiddle(ListNode* head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* middle = findMiddle(head);
+        ListNode* right = middle->next;
+        middle->next = NULL;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+        return mergeTwoSortedLL(left, right);
+    }
