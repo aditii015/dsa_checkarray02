1c4989a3a61a9f235f36aaf7bc47b26e1c36a242
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-16 07:15:18 +0530

    #82 Remove Duplicates from Sorted List II

--- Arrays.cpp
+++ Arrays.cpp
@@ -1125,3 +1125,25 @@ ListNode* deleteDuplicates(ListNode* head) {
         }
         return head;
     }
+
+//82 Remove Duplicates from Sorted List II
+ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+
+        while(curr != nullptr){
+            if(curr->next != nullptr && curr->val == curr->next->val){
+                while(curr->next != nullptr && curr->val == curr->next->val){
+                    curr = curr->next;
+                }
+                prev->next = curr->next;
+            }
+            else{
+                prev = curr;
+            }
+            curr = curr->next;
+        }
+        return dummy->next;
+    }
