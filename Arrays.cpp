0dc3f6481efcda771df2bf666d5fe64f97ee56eb
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-17 07:05:45 +0530

    #25 Reverse Nodes in K-group

--- Arrays.cpp
+++ Arrays.cpp
@@ -1147,3 +1147,36 @@ ListNode* deleteDuplicates(ListNode* head) {
         }
         return dummy->next;
     }
+
+//25 Reverse Nodes in K-group(hard problem)
+ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* groupPrev = dummy;
+        while(true){
+            ListNode* kth = getKthNode(groupPrev, k);
+            if(!kth) break;
+
+            ListNode* groupNext = kth->next;
+            ListNode* prev = groupNext;
+            ListNode* curr = groupPrev->next;
+
+            for(int i=0;i<k;i++){
+                ListNode* temp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = temp; 
+            }
+            ListNode* temp = groupPrev->next;
+            groupPrev->next = kth;
+            groupPrev = temp;
+        }
+        return dummy->next;
+    }
+    ListNode* getKthNode(ListNode* curr, int k){
+        while(curr && k>0){
+            curr = curr->next;
+            k--;
+        }
+        return curr;
+    }
