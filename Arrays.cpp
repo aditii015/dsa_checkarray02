819771bee4c3618f230e2b8407d50dda8916ed10
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-19 07:32:23 +0530

    #17 Letter Combinations of a phone number

--- Arrays.cpp
+++ Arrays.cpp
@@ -1202,3 +1202,32 @@ public:
         return power(x, num);
     }
 };
+
+//17 Letter Combinations of a phone number
+class Solution {
+public:
+    vector<string> result;
+    vector<string> letters = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+    void solve(string& digits, int index, string current){
+        if(index == digits.size()){
+            result.push_back(current);
+            return;
+        }
+        string comb = letters[digits[index] - '0'];
+        for(char ch : comb){
+            current.push_back(ch);
+
+            solve(digits, index + 1, current);
+            current.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return {};
+        solve(digits, 0, "");
+        return result;
+    }
+};
