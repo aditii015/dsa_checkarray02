8216863717e48c4553d1b9a028b9b95e223a934e
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-20 06:51:03 +0530

    $100 Same Tree

--- Arrays.cpp
+++ Arrays.cpp
@@ -1231,3 +1231,15 @@ public:
         return result;
     }
 };
+
+//100 Same Tree
+ bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL) return true;
+        if(p==NULL || q==NULL) return false;
+
+        if(p->val != q->val) return false;
+
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+        return left && right;
+    }
