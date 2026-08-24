3ad53c6dc34de794f4f3a863d3c38c22138daf96
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-24 07:21:16 +0530

    #113 Path Sum II

--- Arrays.cpp
+++ Arrays.cpp
@@ -1335,3 +1335,28 @@ TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         root1->right = mergeTrees(root1->right, root2->right);
         return root1;
     }
+
+//113 Path Sum II (BT)
+void dfs(TreeNode* root, int targetSum, vector<int>& path, vector<vector<int>>& ans){
+        if(root == NULL) return;
+        path.push_back(root->val);
+        targetSum -= root->val;
+
+        if(root->left == NULL && root->right == NULL){
+            if(targetSum == 0){
+               ans.push_back(path);
+            }
+            path.pop_back();
+            return;
+        }   
+        dfs(root->left, targetSum, path, ans);
+        dfs(root->right, targetSum, path, ans);
+        path.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> path;
+
+        dfs(root, targetSum, path, ans);
+        return ans;
+    }
