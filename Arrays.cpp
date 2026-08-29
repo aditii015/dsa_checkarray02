dd3139b560119e201f8cc33528be9841ce982ab5
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-29 06:45:41 +0530

    #56 Merge Intervals

--- Arrays.cpp
+++ Arrays.cpp
@@ -1707,3 +1707,21 @@ int maxArea(vector<int>& height) {
         }
         return area;
     }
+
+//56 Merge Intervals(O(nlong))
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        vector<int> current = intervals[0];
+
+        for(int i=0;i<intervals.size();i++){
+            if(intervals[i][0] <= current[1]){
+                current[1] = max(current[1], intervals[i][1]);
+            }else{
+                ans.push_back(current);
+                current = intervals[i];
+            }
+        }
+        ans.push_back(current);
+        return ans;
+}
