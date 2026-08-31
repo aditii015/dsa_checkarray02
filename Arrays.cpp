794d784c1e06ce524b0aeb7401c3780a44df208a
Aditi Pingoliya <pingolia2007@gmail.com>
2026-08-31 07:23:50 +0530

    #347 Top K Frequent Elements

--- Arrays.cpp
+++ Arrays.cpp
@@ -1803,3 +1803,42 @@ int trap(vector<int>& height) {
         }
         return total;            
     }
+
+//347 Top K Frequent Elements
+//Bruteforce
+vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(int num : nums){
+            mp[num]++;
+        }
+        vector<pair<int, int>> freq;
+        for(auto it : mp){
+            freq.push_back({it.second, it.first});
+        }
+        sort(freq.rbegin(), freq.rend());
+        vector<int> ans;
+        for(int i=0;i<k;i++){
+            ans.push_back(freq[i].second);
+        }
+        return ans;
+    }
+//Optimal(hash map + min heap)
+vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(int num : nums){
+            mp[num]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(auto it : mp){
+            pq.push({it.second, it.first});
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        vector<int> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
