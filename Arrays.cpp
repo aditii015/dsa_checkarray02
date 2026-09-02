6334a2bbfa3f50421c7f52ea59775386da2fa111
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-02 07:28:12 +0530

    #150 Evaluate Reverse Polish Notation

--- Arrays.cpp
+++ Arrays.cpp
@@ -1901,3 +1901,39 @@ public:
         return minSt.top();
     }
 };
+
+//150 Evaluate Reverse Polish Notation
+int evalRPN(vector<string>& tokens) {
+       stack<int> st;
+        for(int i = 0; i < tokens.size(); i++) {
+            if(tokens[i] != "+" &&
+               tokens[i] != "-" &&
+               tokens[i] != "*" &&
+               tokens[i] != "/") {
+
+               st.push(stoi(tokens[i]));
+            }
+            else {
+               int a = st.top();
+               st.pop();
+               int b = st.top();
+               st.pop();
+               int result;
+
+                if(tokens[i] == "+") {
+                   result = b + a;
+                }
+                else if(tokens[i] == "-") {
+                   result = b - a;
+                }
+                else if(tokens[i] == "*") {
+                   result = b * a;
+                }
+                else {
+                   result = b / a;
+                }
+                st.push(result);
+            }
+        }
+        return st.top();
+    }
