009f701c71f8caafdc08fb3e531c6f4383d158dd
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-05 07:23:30 +0530

    #33 Search in Rotated Sorted Array

--- Arrays.cpp
+++ Arrays.cpp
@@ -1989,3 +1989,30 @@ int largestRectangleArea(vector<int>& heights) {
         }
         return maxArea;
     }
+
+//33 Search in Rotated Sorted Array(binary search)
+int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+
+        while(left <= right){
+            int mid = (left+right) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] <= nums[mid]){
+                if( nums[left] <= target && target < nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }else{
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
