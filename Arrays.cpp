3ba7b2b530a3246bd5a63db0390b15c6518fa3a1
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-07 07:50:16 +0530

    #16 3Sum Closest

--- Arrays.cpp
+++ Arrays.cpp
@@ -2089,3 +2089,28 @@ string minWindow(string s, string t) {
         }
         return s.substr(start, minLen);
     }
+
+//16 3Sum Closest(Sort + two pointers)
+int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int ans = nums[0]+nums[1]+nums[2];
+
+        for(int i=0;i<nums.size()-2;i++){
+            int j = i+1;
+            int k = nums.size()-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(abs(sum - target) < abs(ans-target)){
+                    ans = sum;
+                }
+                if(sum < target){
+                    j++;
+                }else if(sum > target){
+                    k--;
+                }else{
+                    return sum;
+                }
+            }
+        }
+        return ans;
+    }
