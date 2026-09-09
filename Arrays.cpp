62e15a633d923a807d88cae777aba301783984bc
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-09 07:26:23 +0530

    #209 Minimum Size Subarray Sum

--- Arrays.cpp
+++ Arrays.cpp
@@ -2133,3 +2133,20 @@ int characterReplacement(string s, int k) {
         }
         return ans;
     }
+
+//209 Minimum Size Subarray Sum(sliding-window)
+int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0;
+        int sum = 0;
+        int ans = INT_MAX;
+
+        for(int right=0;right<nums.size();right++){
+            sum += nums[right];
+            while(sum >= target){
+                ans = min(ans, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
