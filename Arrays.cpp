5972252ded2540396773372df985e832f267835a
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-10 07:13:00 +0530

    #875 Koko Eating Bananas

--- Arrays.cpp
+++ Arrays.cpp
@@ -2150,3 +2150,38 @@ int minSubArrayLen(int target, vector<int>& nums) {
         }
         return ans == INT_MAX ? 0 : ans;
     }
+
+//875 Koko Eating Bananas
+//Bruteforce(TLE)
+int minEatingSpeed(vector<int>& piles, int h) {
+        int maxPile = *max_element(piles.begin(), piles.end());
+        for(int k=1;k<=maxPile;k++){
+            long long hours = 0;
+            for(int pile : piles){
+                hours += (pile + k - 1)/k;
+            }
+            if(hours <= h){
+                return k;
+            }
+        }
+        return maxPile;
+    }
+//Optimal (Binary Search)
+int minEatingSpeed(vector<int>& piles, int h) {
+        int maxPile = *max_element(piles.begin(), piles.end());
+        int left = 1;
+        int right = maxPile;
+        while(left <= right){
+            int mid = (left + right)/2;
+            long long hours = 0;
+            for(int pile : piles){
+               hours += (pile + mid - 1)/mid;
+            }
+            if(hours > h){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
