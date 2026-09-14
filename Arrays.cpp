5b2f2a37e56b6b3e40c419f919ce3672941ab1ee
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-14 17:30:15 +0530

    #344 Reverse String

--- Arrays.cpp
+++ Arrays.cpp
@@ -2278,3 +2278,15 @@ vector<int> findAnagrams(string s, string p) {
         }
         return ans;
     }
+
+//344 Reverse String(two pointer optimal)
+void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right){
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
