16567b779ac20399e6d32e480087b2efd36fa7dd
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-15 17:32:33 +0530

    #387 First Unique Character in a string

--- Arrays.cpp
+++ Arrays.cpp
@@ -2290,3 +2290,34 @@ void reverseString(vector<char>& s) {
             right--;
         }
     }
+
+//387 First Unique Character in a string
+//Bruteforce(TLE)
+int firstUniqChar(string s) {
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            int count = 0;
+            for(int j=0;j<n;j++){
+                if(s[i] == s[j]){
+                    count++;
+                }
+            }
+            if(count == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+//Optimal
+int firstUniqChar(string s) {
+        int freq[26] = {};
+        for(char c : s){
+            freq[c - 'a']++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i] - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
