3a3a26b6bb9cfc7f52a4fb7e2a23569524a8a79d
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-18 16:36:00 +0530

    #42 Group Anagrams

--- Arrays.cpp
+++ Arrays.cpp
@@ -2348,3 +2348,18 @@ int compress(vector<char>& chars) {
         }
         return write;
     }
+
+//42 Group Anagrams(optimal - hash map)
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for(string word : strs){
+            string key = word;
+            sort(key.begin(), key.end());
+            mp[key].push_back(word);
+        }
+        vector<vector<string>> ans;
+        for (auto& entry : mp){
+            ans.push_back(entry.second);
+        }
+        return ans;
+    }
