d04cd529653ffde17314aac57904a6eaddb43f04
Aditi Pingoliya <pingolia2007@gmail.com>
2026-09-24 07:33:49 +0530

    #557 Reverse words in String III

--- Arrays.cpp
+++ Arrays.cpp
@@ -2467,3 +2467,16 @@ int smallestIndex(vector<int>& nums) {
         }
         return -1;
     }
+
+//557 Reverse words in String III
+string reverseWords(string s) {
+        int start = 0;
+
+        for(int i=0;i<=s.size();i++){
+            if(i == s.size() || s[i] == ' '){
+                reverse(s.begin() + start, s.begin() + i);
+                start = i + 1;
+            }
+        }
+        return s;
+    }
